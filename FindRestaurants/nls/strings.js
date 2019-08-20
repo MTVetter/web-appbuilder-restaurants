0d336463c832f27d47b05422003d4bee1e5b82cf
MTVetter <mvetter2@wisc.edu>
2019-08-20 11:58:12 -0500

    removed FindRestaurants folder
    FindRestaurants folder is located in the widgets folder so it was a duplicate

--- FindRestaurants/nls/strings.js
+++
@@ -1,8 +0,0 @@
-/*global define*/
-define({
-  root: ({
-    _widgetLabel: "Find Restaurants", // widget label shown on the choose widget dialog and widget panel title
-    welcomeLine: "Hello!",
-    _featureAction_RestaurantsNear: "Find Restaurants"
-  })
-});

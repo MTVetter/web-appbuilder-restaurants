cfb2c77695da7405223b1a49c579fa56d9d323bb
MTVetter <mvetter2@wisc.edu>
2019-08-20 13:22:48 -0500

    Update yelp.js

--- widgets/FindRestaurants/yelp.js
+++ widgets/FindRestaurants/yelp.js
@@ -19,7 +19,7 @@ define([
 function(
   declare, BaseWidget, esriRequest, InfoTemplate, FeatureLayer, InfoTemplate, esriConfig, Point, SimpleMarkerSymbol, SimpleLineSymbol, PictureMarkerSymbol, Color, Graphic, GraphicsLayer, SpatialReference, lang
 ) {
-  return declare([BaseWidget], {
+  return declare([], {
     apiKey: '',
     apiSearchUrl:'https://api.yelp.com/v3/businesses/search',
 
